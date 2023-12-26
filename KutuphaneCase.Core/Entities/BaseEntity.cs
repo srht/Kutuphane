f3f08323eb07a81a93ba9895ce0ccd6f06b56408
Serhat Köroğlu <srhtkrgl@outlook.com>
2023-12-26 14:53:22 +0300

    Add project files.

---
+++ KutuphaneCase.Core/Entities/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KutuphaneCase.Core.Entities
+{
+    public class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+
+        public BaseEntity()
+        {
+            this.CreatedDate = DateTime.UtcNow;
+            this.ModifiedDate = DateTime.UtcNow;
+        }
+    }
+}
---
+++ KutuphaneCase.Core/Entities/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KutuphaneCase.Core.Entities
+{
+    public class Book:BaseEntity
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string URL { get; set; }
+        public bool IsBorrowed { get; set; }=false;
+        public DateTime? ReturnDate { get; set; }
+    }
+}
